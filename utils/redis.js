304d2dc4439c7111fe32ea90afde60ca15dfe372
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-09 11:16:26 +0100

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+        console.error(err);
+      });
+    console.log('client is: ', this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve('OK');
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
