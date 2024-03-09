994768318e215ebc38df64498cc50f377df0ab4c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-09 16:18:36 +0100

    Defined callback function for handling the POST /user endpoint

---
+++ controllers/UsersController.js
@@ -0,0 +1,49 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+export default function postNew(req, res) {
+  const { email, password } = req.body;
+
+  if (email === undefined) {
+    res.status(400).send('Missing email');
+  }
+
+  if (password === undefined) {
+    res.status(400).send('Missing password');
+  }
+
+  const collection = dbClient.client.db(dbClient.database).collection('users');
+
+  /**
+   * const user = collection('users').find({ email: email }).toArray();
+   * if (user.length !== 0) {
+   * res.status(400).send('Already exist');
+   * } else {
+   */
+
+  collection.findOne({ email }, (error, user) => {
+    if (error) {
+      res.status(500).send('Internal server error');
+      return;
+    }
+
+    if (user) {
+      res.status(400).send('Already exist');
+    } else {
+      const sha1Hash = crypto.createHash('sha1');
+
+      sha1Hash.update(password);
+
+      const hashedPassword = sha1Hash.digest('hex');
+
+      collection.insertOne({ email, password: hashedPassword },
+        (error, result) => {
+          if (error) {
+            console.error('Error inserting document: ', error);
+          } else {
+            res.status(201).json({ id: result.insertedId, email });
+          }
+        });
+    }
+  });
+}
