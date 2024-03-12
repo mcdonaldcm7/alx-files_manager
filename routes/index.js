c6573d9117c6b4b3a9e39bd1b96459d5d95ba5bb
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-12 04:26:00 +0100

    Added 3 new endpoints: POST /files, GET /files/:id, GET /files

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
 import { postNew, getMe } from '../controllers/UsersController';
 import { getConnect, getDisconnect } from '../controllers/AuthController';
+import { postUpload, getShow, getIndex } from '../controllers/FilesController';
 
 const router = Router();
 
@@ -11,5 +12,8 @@ router.post('/users', postNew);
 router.get('/connect', getConnect);
 router.get('/disconnect', getDisconnect);
 router.get('/users/me', getMe);
+router.post('/files', postUpload);
+router.get('/files/:id', getShow);
+router.get('/files', getIndex);
 
 export default router;
